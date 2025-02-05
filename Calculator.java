111390dc8a3f1fca5fb8a279246c2e3c55152074
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-05 15:26:23 +0530

    add new feature

--- Calculator.java
+++ Calculator.java
@@ -10,6 +10,17 @@ class Operation
     {
         return a-b;
     }
+
+    public int mul(int a,int b)
+    {
+        return a*b;
+    }
+
+    public int div(int a,int b)
+    {
+        return b/a;
+
+    }
 }
 
 public class Calculator
@@ -17,6 +28,15 @@ public class Calculator
     public static void main(String[] args) {
          Operation  operation=new Operation();
          int result=operation.add(2, 3);
-         System.out.println(result);
+         System.out.println("addtion is:"+result);
+
+         int resultSub=operation.sub(12, 10);
+         System.out.println("substraction is:"+resultSub);
+
+         int result3=operation.mul(12, 12);
+         System.out.println("multiplication is:"+result3);
+
+         int result4=operation.div(4, 16);
+         System.out.println("division is :"+result4);
     }
 }
